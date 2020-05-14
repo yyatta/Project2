69f73a8d120f22e190ad7c88d03ba17b67258489
yyatta <55525589+yyatta@users.noreply.github.com>
2020-05-14 23:09:30 +0900

    Update 3r.cpp

--- Project2/3r.cpp
+++ Project2/3r.cpp
@@ -1,18 +1,79 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void)
-{
-	double a = 0, b = 0;
+typedef char element;
 
-	printf("»ê¼ú¿¬»êÀ» ¼öÇàÇÒ µÎ ½Ç¼ö¸¦ ÀÔ·ÂÇÏ¼¼¿©\n");
-	scanf_s("%lf, %lf,", &a, &b);
+typedef struct stackNode {
+	element data;
+	struct stackNode* link;
+} stackNode;
 
-	printf("%8.2f + %8.2f ++> %8.2f\n", a, b, a + b);
-	printf("%08.2f - %08.2f ==> %8.2f\n", a, b, a - b);
-	printf("%+8.2f * %8.2f ==> %8.2f\n", a, b, a * b);
-	printf("%-8.2f / %-8.2f ==> %8.2f\n", a, b, a / b);
+stackNode* top;
 
-	return 0;
+int isEmpty() {
+	if (top == NULL) return 1;
+	else return 0;
+}
+
+void push(element item) {
+	stackNode* temp = (stackNode*)malloc(sizeof(stackNode));
+	temp->data = item;
+	temp->link = top;
+	top = temp;
+}
+
+element pop() {
+	element item;
+	stackNode* temp = top;
+
+	if (top == NULL) {
+		printf("\n\n Stack is empty !\n");
+		return 0;
+	}
+	else {
+		item = temp->data;
+		top = temp->link;
+		free(temp);
+		return item;
+	}
+}
+int testPair(char* exp) {
+	char symbol, open_pair;
+	int i, length = strlen(exp);
+	top = NULL;
+
+	for (i = 0; i < length; i++) {
+		symbol = exp[i];
+		switch (symbol) {
+		case '(':
+		case '[':
+		case '{':
+			push(symbol); break;
+		case ')':
+		case ']':
+		case '}':
+			if (top == NULL) return 0;
+			else {
+				open_pair = pop();
+				if ((open_pair == '(' && symbol != ')') || (open_pair == '[' && symbol != ']') || (open_pair == '{' && symbol != '}'))
+					return 0;
+				else break;
+			}
+		}
+	}
+	if (top == NULL) return 1;
+	else return 0;
+}
+
+void main(void) {
+	char* express = "{(A+B)-3}*5+[{cos(x+y)+7}-1]*4";
+	printf("%s", express);
+
+	if (testPair(express) == 1)
+		printf("\n\nìˆ˜ì‹ì˜ ê´„í˜¸ê°€ ë§žê²Œ ì‚¬ìš©ë˜ì—ˆìŠµë‹ˆë‹¤!");
+	else
+		printf("\n\nìˆ˜ì‹ì˜ ê´„í˜¸ê°€ í‹€ë ¸ìŠµë‹ˆë‹¤!");
+
+	getchar();
 }
